d77aaa80f0a74c4a1ef767ff1fa877c1a3ae22df
MelvaMateo <odany_m@unitec.edu>
2026-08-24 21:50:07 -0600

    refactor(practica-5): paso 4/5 - extraer funciones y separar responsabilidades (principio 1 y 8)

--- practica-5/sano.js
+++ practica-5/sano.js
@@ -2,36 +2,61 @@ const UMBRAL_DESCUENTO = 500;
 const PORCENTAJE_DESCUENTO = 0.1;
 const TASA_IMPUESTO = 0.15;
 
+function validarDisponibilidad(items, inventario) {
+          for (var idx = 0; idx < items.length; idx++) {
+                      if (items[idx].cant <= 0) return false;
+                      if (inventario[items[idx].id] < items[idx].cant) return false;
+          }
+          return true;
+}
+
+function calcularSubtotal(items) {
+          var subtotal = 0;
+          for (var idx = 0; idx < items.length; idx++) {
+                      subtotal = subtotal + items[idx].precio * items[idx].cant;
+          }
+          return subtotal;
+}
+
+function calcularDescuento(subtotal) {
+          return subtotal > UMBRAL_DESCUENTO ? subtotal * PORCENTAJE_DESCUENTO : 0;
+}
+
+function actualizarInventario(items, inventario) {
+          for (var idx2 = 0; idx2 < items.length; idx2++) {
+                      inventario[items[idx2].id] = inventario[items[idx2].id] - items[idx2].cant;
+          }
+}
+
+function generarRecibo(cliente, subtotal, descuento, impuesto, total, items) {
+          var recibo = "Cliente: " + cliente + "\n";
+          recibo = recibo + "Subtotal: " + subtotal + "\n";
+          recibo = recibo + "Descuento: " + descuento + "\n";
+          recibo = recibo + "Impuesto: " + impuesto + "\n";
+          recibo = recibo + "Total: " + total + "\n";
+          if (cliente == "mayorista") {
+                      var totalMayorista = 0;
+                      for (var idx3 = 0; idx3 < items.length; idx3++) {
+                                    totalMayorista = totalMayorista + items[idx3].precio * items[idx3].cant;
+                      }
+                      recibo = recibo + "Total mayorista: " + totalMayorista + "\n";
+          }
+          return recibo;
+}
+
 function registrarVenta(items, cliente, inventario) {
-        var subtotal = 0;
-        for (var idx = 0; idx < items.length; idx++) {
-                  if (items[idx].cant <= 0) return null;
-                  if (inventario[items[idx].id] < items[idx].cant) return null;
-                  subtotal = subtotal + items[idx].precio * items[idx].cant;
-        }
+          if (!validarDisponibilidad(items, inventario)) return null;
 
-  var descuento = subtotal > UMBRAL_DESCUENTO ? subtotal * PORCENTAJE_DESCUENTO : 0;
-        var subtotalConDescuento = subtotal - descuento;
-        var impuesto = subtotalConDescuento * TASA_IMPUESTO;
-        var total = subtotalConDescuento + impuesto;
+  var subtotal = calcularSubtotal(items);
+          var descuento = calcularDescuento(subtotal);
+          var subtotalConDescuento = subtotal - descuento;
+          var impuesto = subtotalConDescuento * TASA_IMPUESTO;
+          var total = subtotalConDescuento + impuesto;
 
-  for (var idx2 = 0; idx2 < items.length; idx2++) {
-            inventario[items[idx2].id] = inventario[items[idx2].id] - items[idx2].cant;
-  }
+  actualizarInventario(items, inventario);
 
-  var recibo = "Cliente: " + cliente + "\n";
-        recibo = recibo + "Subtotal: " + subtotal + "\n";
-        recibo = recibo + "Descuento: " + descuento + "\n";
-        recibo = recibo + "Impuesto: " + impuesto + "\n";
-        recibo = recibo + "Total: " + total + "\n";
-        if (cliente == "mayorista") {
-                  var totalMayorista = 0;
-                  for (var idx3 = 0; idx3 < items.length; idx3++) {
-                              totalMayorista = totalMayorista + items[idx3].precio * items[idx3].cant;
-                  }
-                  recibo = recibo + "Total mayorista: " + totalMayorista + "\n";
-        }
-        return { total: total, recibo: recibo };
+  var recibo = generarRecibo(cliente, subtotal, descuento, impuesto, total, items);
+          return { total: total, recibo: recibo };
 }
 
 module.exports = { registrarVenta };
